d5f12498dc077372bfa515a8ac42ba57fd5e0501
Saul <saul.crespo.sc@gmail.com>
2023-04-24 09:20:10 +0200

    La correción a lo que dijo mi esposa =) Te amo

--- src/HOla/Hola.java
+++ src/HOla/Hola.java
@@ -3,5 +3,7 @@ package HOla;
 public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Keyla y yo te amo más y todo el mundo lo sabe");
+		System.out.println("Hmmmmm, no se yo, no se yo. Creo que todo el mundo sabe que yo te amo mas.");
+		System.out.println("\t\t\tS&K");
 	}
 }
